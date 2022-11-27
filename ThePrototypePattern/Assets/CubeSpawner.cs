9287a0cb58d750bfaaf89eba0879abc499c3c940
Mert <mertozata009@gmail.com>
2022-11-27 22:15:30 +0300

    ThePrototypePattern
    ThePrototypePattern

---
+++ ThePrototypePattern/Assets/CubeSpawner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    void Update()
+    {
+        if (Random.Range(0, 100) < 10)
+        {
+            ProcCube.Clone(this.transform.position);
+        }
+    }
+}
